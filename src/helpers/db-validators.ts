0dc0b6ebb38694ab093f9e5b240cce08ad57efbd
Paul Miranda Vega <paul.miranda@sparktechs.com>
2021-09-25 16:16:23 -0700

    added is user active helper

--- src/helpers/db-validators.ts
+++ src/helpers/db-validators.ts
@@ -6,7 +6,11 @@ export const userExists = async (id: string) => {
     if (!user) {
         throw new Error(`The id doesn't exists`);
     }
-    if (user.state === false) {
+}
+
+export const isUserActive = async (id: string) => {
+    const user = await User.findById(id);
+    if (user && user.state === false) {
         throw new Error(`The user isn't active`);
     }
 }
